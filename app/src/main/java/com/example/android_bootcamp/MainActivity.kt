7be3b296c003ea00b441bb543680693e3b81b9fa
emromerog <emromerog@unal.edu.co>
2022-04-28 11:32:54 -0500

    feat: add files to create calculator

--- app/src/main/java/com/example/android_bootcamp/MainActivity.kt
+++ app/src/main/java/com/example/android_bootcamp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.android_bootcamp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun allClearAction(view: View) {
+        //workingsTV.text = ""
+        //resultsTV.text = ""
+    }
+
+    fun backSpaceAction(view: View) {}
+    fun equalsAction(view: View) {}
 }
