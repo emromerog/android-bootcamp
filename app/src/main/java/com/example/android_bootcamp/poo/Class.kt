32657b09045efbaa1cb21df54a99f79c60e728f0
emromerog <emromerog@unal.edu.co>
2022-05-03 14:49:33 -0500

    feat: add class

---
+++ app/src/main/java/com/example/android_bootcamp/poo/Class.kt
@@ -0,0 +1,15 @@
+package com.example.android_bootcamp.poo
+
+/*Por defecto las clases en kotlin son Final, una data class contiene una funcionalidad estandar
+* */
+data class DataClass(var temperature: Int = 0) {
+}
+
+class Class {
+
+}
+
+open class OpenClass {
+
+}
+
